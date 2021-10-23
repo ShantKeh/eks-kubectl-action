2ac8a83163acbd7388699e2a2f0fce05d7e78251
Shant Kehiaian <shant.kehiaian@blackline.com>
2021-10-23 11:32:36 -0700

    attempt to add support for aws session token

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@ set -e
 
 export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
+export AWS_SESSION_TOKEN="$INPUT_AWS_SESSION_TOKEN"
 
 echo "aws version"
 
 aws --version
+
+aws configure list
 
 echo "Attempting to update kubeconfig for aws"
 
